8763b292860ac7a083b492bc1cc423fd1bc8cd1e
AMIR  HASSAN <amirhassan@AMIRs-MacBook-Air.local>
2018-10-04 23:10:57 -0400

    update

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,23 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burger_db"
-});
-
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "burger_db"
+if (process.env.JANSDB_URL) {
+    connection = mysql.createConnection(process.env.JANSDB_URL)
+}
+else {
+    connection =mysql.createConnection({
+        host: "localhost",
+          port: 3306,
+          user: "root",
+          password: "",
+          database: "burger_db"
+    }) 
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
